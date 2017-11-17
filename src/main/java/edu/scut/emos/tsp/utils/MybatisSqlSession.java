d1b1e592e12158df6bad369a0e606b2c50050955
imppresser <503731351@qq.com>
2017-11-17 00:14:52 -0800

    add mybatis component

---
+++ src/main/java/edu/scut/emos/tsp/utils/MybatisSqlSession.java
@@ -0,0 +1,37 @@
+package edu.scut.emos.tsp.utils;
+/**
+ * @author Qinzheng
+ */
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class MybatisSqlSession {
+
+	private static String resource = "mybatis-config.xml";
+
+	private static SqlSessionFactory sqlSessionFactory;
+
+	public static synchronized SqlSessionFactory getSqlSessionFactory() {
+		if (sqlSessionFactory == null) {
+			InputStream inputStream;
+			try {
+				inputStream = Resources.getResourceAsStream(resource);
+				sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return sqlSessionFactory;
+	}
+
+	public static synchronized SqlSession openSession() {
+		return getSqlSessionFactory().openSession();
+	}
+}
